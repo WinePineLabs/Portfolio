d7750dfe14bbf9bdc76702ff0b8c961671c2bba8
winepine Zz <basitsaeed1@gmail.com>
2020-08-05 17:46:38 -0700

    "Added Background And Black Layer"

---
+++ src/components/background.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './background.css'
+export default class Background extends React.Component{
+    render(){
+        return(
+            <div className="background">
+                <div className="black-layer">
+                    
+                </div>
+            </div>
+        )
+    }
+}
