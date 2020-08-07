f01443f0f12a1bd9e19a88726ec11c968b63989c
winepine Zz <basitsaeed1@gmail.com>
2020-08-07 14:12:47 -0700

    "added description"

--- src/components/home.js
+++ src/components/home.js
@@ -6,6 +6,7 @@ export default class Home extends React.Component{
         return(
             <div className='home'>
                 <h1>Basit <br/> Saeed </h1>
+                <h2 className='white'>Front-End </h2><h2 className='brown'>Engineeer</h2><h2 className='white'> & Open Source</h2><h2 className='brown'> Contributor</h2>
             </div>
         )
     }
