6bf1e0806eeb56274ce1f1bdf1cace04c81870a2
winepine Zz <basitsaeed1@gmail.com>
2020-08-07 00:46:17 -0700

    Added Animation And Toggle Button

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,16 +1,46 @@
 import React from 'react';
 import './styles.css'
+import burger from '../images/menu.png'
 export default class Nav extends React.Component{
+    state={
+        toggled:false,
+        current:'.nothomebutton'
+    }
+    toggle=()=>{
+        const menu=document.querySelector(this.state.current);
+        const navitems=document.querySelectorAll('.nav-item');
+        if(this.state.toggled){
+            navitems.forEach(item=>{
+                item.style.animation = "none";
+            })
+            this.setState({toggled:false,current:'.nothomebutton'});
+            menu.className='nothomebutton';
+        }else{
+            navitems.forEach((item,idx)=>{
+                console.log(idx)
+                item.style.animation=`comein ${(idx+3)/5}s`
+            })
+            this.setState({toggled:true,current:'.nothomebutton-active'});
+            menu.className='nothomebutton-active'
+        }
+    }
     render(){
         return(
             <div className='navbar'>
-                <h2>Home</h2>
-                <ul>
-                    <li><button>Projects</button></li>
-                    <li><button>Education</button></li>
-                    <li><button>Skills</button></li>
-                    <li><button>About</button></li>
-                </ul>
+                <div className='homebutton'>
+                    <button>Home</button>
+                </div>
+                <div className='nothomebutton'>
+                    <button className='nav-item'>Projects</button>
+                    <button className='nav-item'>Education</button>
+                    <button className='nav-item'>Skills</button>
+                    <button className='nav-item'>About</button>
+                </div>
+                <div className='toggle-menu'>
+                    <button onClick={this.toggle}>
+                    <img src={burger} height="30px" alt=""/>
+                    </button>
+                </div>
             </div>
         )
     }
