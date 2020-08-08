3fa49086c13954e5bc2d5d0d586a01f987f59f58
winepine Zz <basitsaeed1@gmail.com>
2020-08-08 11:57:07 -0700

    onmousemove movement

--- src/components/home.js
+++ src/components/home.js
@@ -2,9 +2,21 @@ import React from 'react'
 import './styles.css'
 
 export default class Home extends React.Component{
+    
+    postion=(e)=>{
+        var x=e.clientX;
+        var y=e.clientY;
+        //console.log(x,y);
+        const toBeMoved=document.querySelector('.home').childNodes;
+        toBeMoved.forEach((element,idx)=>{
+            element.style.marginLeft=`${x/(150*(idx+1))}px`;
+            element.style.marginTop=`${y/(150*(idx+1))}px`;
+        })
+    }
+
     render(){
         return(
-            <div className='home'>
+            <div onMouseMove={this.postion} className='home'>
                 <h1>Basit <br/> Saeed </h1>
                 <div className='description'>
                 <h2 className='white'>Front-End </h2><h2 className='brown'>Dev</h2><h2 className='white'> & Open Source</h2><h2 className='brown'> Contributor</h2>
