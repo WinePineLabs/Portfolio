9ed6d1083e2f9571dd7368f7c42079b04520e8f8
winepine Zz <basitsaeed1@gmail.com>
2020-08-07 17:08:54 -0700

    Landsacpe mode and iPad mode adjustments

--- src/components/home.js
+++ src/components/home.js
@@ -6,7 +6,9 @@ export default class Home extends React.Component{
         return(
             <div className='home'>
                 <h1>Basit <br/> Saeed </h1>
-                <h2 className='white'>Front-End </h2><h2 className='brown'>Engineeer</h2><h2 className='white'> & Open Source</h2><h2 className='brown'> Contributor</h2>
+                <div className='description'>
+                <h2 className='white'>Front-End </h2><h2 className='brown'>Dev</h2><h2 className='white'> & Open Source</h2><h2 className='brown'> Contributor</h2>
+                </div>
             </div>
         )
     }
