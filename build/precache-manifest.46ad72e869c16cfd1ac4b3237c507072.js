3b3971992f112e0ab5617002cf80d15e784dc4aa
winepine Zz <basitsaeed1@gmail.com>
2020-08-10 20:43:42 -0700

    build add

---
+++ build/precache-manifest.46ad72e869c16cfd1ac4b3237c507072.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "652a23ed70ce8ef6a3860c7c8f8e2fbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4df8b918f5812bb02dd",
+    "url": "/static/css/main.6b0d76bf.chunk.css"
+  },
+  {
+    "revision": "ccb68c0f9ce85ab60477",
+    "url": "/static/js/2.48ebb3cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.48ebb3cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4df8b918f5812bb02dd",
+    "url": "/static/js/main.08d934d5.chunk.js"
+  },
+  {
+    "revision": "3f9cdb1d109962be5fc3",
+    "url": "/static/js/runtime-main.a1b2fe39.js"
+  },
+  {
+    "revision": "8ba765ce9580a3bdae76b59130d0ef8a",
+    "url": "/static/media/AmaticSC-Bold.8ba765ce.ttf"
+  },
+  {
+    "revision": "544fa4f2678a8285eb88b8dfe503c90c",
+    "url": "/static/media/Poppins-ExtraBold.544fa4f2.ttf"
+  },
+  {
+    "revision": "8b6af8e5e8324edfd77af8b3b35d7f9c",
+    "url": "/static/media/Poppins-Regular.8b6af8e5.ttf"
+  },
+  {
+    "revision": "592e30e579e2ebb7b9d48d830741f668",
+    "url": "/static/media/background.592e30e5.jpg"
+  },
+  {
+    "revision": "41c270db43e9a70a64b2c16378df62ae",
+    "url": "/static/media/menu.41c270db.png"
+  }
+]);
